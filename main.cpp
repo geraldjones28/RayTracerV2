997af470f83b9b14125ab1400e01fc5aeeb67055
GeraldJones <gajone12@asu.edu>
2025-04-24 03:30:10 -0700

    Added point.h so I can create point vectors. w is hardcoded to 1 so I know it's a point object. Better to separate point and vector objects into different header files.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "point.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    point p(1, 2, 2);
+    // p.e[0] = 1.0f;
+    p.print();
+
     return 0;
 }
---
+++ vec3.h
@@ -0,0 +1,14 @@
+//
+// Created by Gerald Jones on 4/23/25.
+//
+
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <iostream>
+
+using namespace std;
+
+
+
+#endif //VEC3_H
