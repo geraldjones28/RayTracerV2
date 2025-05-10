26e58ebf80a338520caae0b6c5237f0c69212976
GeraldJones <gajone12@asu.edu>
2025-05-09 19:54:50 -0700

    Started working on a 4d matric header file

---
+++ 4dmat.h
@@ -0,0 +1,19 @@
+//
+// Created by Gerald Jones on 5/9/25.
+//
+
+#include <iostream>
+#include <cmath>
+#include <cassert>
+
+using namespace std;
+
+class mat4d {
+    public:
+
+};
+
+#ifndef INC_4DMAT_H
+#define INC_4DMAT_H
+
+#endif //INC_4DMAT_H
